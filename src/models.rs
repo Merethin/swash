9cde0dad3b557088d4adb5a7c0c816374e0ac0e9
Merethin <207838891+Merethin@users.noreply.github.com>
2026-02-08 16:08:06 +0100

    Fix duplicates again

--- src/models.rs
+++ src/models.rs
@@ -141,7 +141,8 @@ impl Nation {
                 $16,$17,$18,$19,$20,$21,$22,
                 $23,$24,$25,$26,$27,$28,$29,
                 $30,$31,$32,$33,$34,$35,$36,
-                $37,$38,$39) ON CONFLICT DO UPDATE SET
+                $37,$38,$39) ON CONFLICT (canon_name) DO UPDATE SET
+                dbid = EXCLUDED.dbid,
                 name = EXCLUDED.name,
                 canon_name = EXCLUDED.canon_name,
                 classification = EXCLUDED.classification,
